9329fd6f242ff1595ceabbb31f042be32bf03f8c
LauriKauhanen <H4215@student.jamk.fi>
2016-04-04 16:15:28 +0300

    Initial commit with project structure

---
+++ cautious-fortnight/cautious-fortnight/OCR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+
+namespace cautious_fortnight
+{
+    class OCR
+    {
+        private Tesseract _ocr;
+    }
+}
